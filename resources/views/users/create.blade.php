518109f88c1cedcbe2c3902851bd0d6d8fa57296
andiankas <rustiantoandi17@gmail.com>
2019-09-17 01:41:22 +0700

    create user - done

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,6 +2,13 @@
 @section("title") Create User @endsection
 @section("content")
 <div class="col-md-8">
+	
+	@if(session('status'))
+  <div class="alert alert-success">
+    {{session('status')}}
+	</div>
+	@endif
+
 	<form enctype="multipart/form-data" class="bg-white shadow-sm p-3" action="{{route('users.store')}}" method="POST">
 		@csrf
 		<label for="name">Name</label>
