240e83c2d46d57945497fb1851dfa5be951657a8
andiankas <rustiantoandi17@gmail.com>
2019-09-17 01:18:26 +0700

    styling dashboard, and login page

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(AdministratorSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
