a4fcfdc2a277a7f48d92ce7fb50e64fd4e6df395
andiankas <rustiantoandi17@gmail.com>
2019-09-17 22:16:19 +0700

    search by email and active or not-active

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,27 +4,27 @@
 <h1>List User</h1>
 <!-- validation message -->
 @if(session('status'))
-  <div class="alert alert-success">
-    {{session('status')}}
-  </div>
-@endif
-
-<br>
-<div class="row">
-	<div class="col-md-6">
-		<a href="{{route('users.create')}}" class="btn btn-md btn-primary">Add New User</a>
-	</div>
-	<div class="col-md-6 text-right">
-		<form action="{{route('users.index')}}">
-			<div class="input-group mb-3">
-				<input value="{{Request::get('keyword')}}" name="keyword" class="form-control col-md-10" type="text" placeholder="Filter berdasarkan email"/>
-				<div class="input-group-append">
-					<input type="submit" value="Filter" class="btn btn-primary">
-				</div>
-			</div>
-		</form>
-	</div>
+<div class="alert alert-success">
+	{{session('status')}}
 </div>
+@endif
+<a href="{{route('users.create')}}" class="btn btn-md btn-primary">Add New User</a>
+<br><br>
+<form action="{{route('users.index')}}">
+	<div class="row">
+		<div class="col-md-6">
+			<input value="{{Request::get('keyword')}}" name="keyword" class="form-control" type="text" placeholder="Masukan email untuk filter..."/>
+		</div>
+		<div class="col-md-6">
+			<input {{Request::get('status') == 'ACTIVE' ? 'checked' : ''}} value="ACTIVE" name="status" type="radio" class="form-control" id="active">
+			<label for="active">Active</label>
+			<input {{Request::get('status') == 'INACTIVE' ? 'checked' : ''}} value="INACTIVE" name="status" type="radio" class="form-control" id="inactive">
+			<label for="inactive">Inactive</label>
+			<input type="submit" value="Filter" class="btn btn-primary">
+		</div>
+	</div>
+</form>
+<br>
 <table class="table table-bordered">
 	<thead>
 		<tr>
@@ -32,6 +32,7 @@
 			<th><b>Username</b></th>
 			<th><b>Email</b></th>
 			<th><b>Avatar</b></th>
+			<th><b>Status</b></th>
 			<th><b>Action</b></th>
 		</tr>
 	</thead>
@@ -49,12 +50,23 @@
 				@endif
 			</td>
 			<td>
+				@if($user->status == "ACTIVE")
+				<span class="badge badge-success">
+					{{ $user->status }}
+				</span>
+				@else
+				<span class="badge badge-danger">
+					{{ $user->status }}
+				</span>
+				@endif
+			</td>
+			<td>
 				<a class="btn btn-info text-white btn-sm" href="{{route('users.edit', [$user->id])}}">Edit</a>
 				<a href="{{route('users.show', [$user->id])}}" class="btn btn-primary btn-sm">Detail</a>
 				<form onsubmit="return confirm('Delete this user permanently?')" class="d-inline" action="{{route('users.destroy', [$user->id])}}" method="POST">
 					@csrf
-				    <input type="hidden" name="_method" value="DELETE">
-				    <input type="submit" value="Delete" class="btn btn-danger btn-sm">
+					<input type="hidden" name="_method" value="DELETE">
+					<input type="submit" value="Delete" class="btn btn-danger btn-sm">
 				</form>
 			</td>
 		</tr>
