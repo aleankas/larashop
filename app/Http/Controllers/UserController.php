d07f64e1b1075a955f32ee24b002ffadb989fe20
andiankas <rustiantoandi17@gmail.com>
2019-09-17 16:26:55 +0700

    show or detail user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -65,7 +65,7 @@ class UserController extends Controller
     public function show($id)
     {
         $user = \App\User::findOrFail($id);
-        return view('users.edit', ['user' => $user]);
+        return view('users.show', ['user' => $user]);
     }
 
     /**
